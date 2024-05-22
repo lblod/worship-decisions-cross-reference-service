b4762ee6336952487233b0e944d9b7f0a35b63c6
felix <ruiz.felixruiz@gmail.com>
2024-05-22 17:01:10 +0200

    added error handling

--- app.js
+++ app.js
@@ -8,46 +8,55 @@ app.get('/hello', function( req, res ) {
 } );
 
 app.get('/related-document-information', async function( req, res ) {
+  try {
+    // Get the related decisions for specific type & bestuurseenheid provided in the query parameters `?forDecisionType=..&?forEenheid=...
+    const decisionType = req.query.forDecisionType;
+    const forEenheidUri = req.query.forEenheid;
 
-  // Get the related decisions for specific type & bestuurseenheid provided in the query parameters `?forDecisionType=..&?forEenheid=...
-  const decisionType = req.query.forDecisionType;
-  const forEenheidUri = req.query.forEenheid;
+    //TODO add ?forDecision
 
-  //TODO add ?forDecision
+    // Check if both required parameters are provided
+    if (!decisionType || !forEenheidUri) {
+      return res.status(400).json({
+        error: "Missing required query parameters. Please provide 'forDecisionType' and 'forEenheid'."
+      });
+    }
 
-  // Check if both required parameters are provided
-  if (!decisionType || !forEenheidUri) {
-    return res.status(400).json({
-      error: "Missing required query parameters. Please provide 'forDecisionType' and 'forEenheid'."
-    });
+    // extract from the `mu-session-id` the bestuurseenheid the user is asking for (i.e. security measure)
+    const sessionUri = req.headers['mu-session-id'];
+
+    if (!sessionUri ) {
+      return res.status(400).json({
+        error: "Missing mu-session-id. This call should go through mu-identifier."
+      });
+    }
+
+    const fromEenheidUri = await bestuurseenheidForSession(sessionUri);
+    if(!fromEenheidUri) {
+      return res.status(400).json({
+        error: "No eenheid found for mu-session-id. Aborting"
+      });
+    }
+
+    // Figure out whether Eenheid is related to CKB
+    const ckbUri = await getRelatedToCKB( forEenheidUri );
+
+    // Get decision type to request
+    const relatedDecisionType = getRelatedDecisionType( decisionType, ckbUri );
+
+    const query = prepareQuery(fromEenheidUri, forEenheidUri, ckbUri, relatedDecisionType );
+
+    // execute query
+    // TODO: Here we could add a hook to connect to vendor-API if we want.
+    const triples = (await querySudo(query))?.results?.bindings || [];
+    const nTriples = triples.map(t => serializeTriple(t)) || [];
+
+    res.set('Content-Type', 'text/turtle');
+    return res.send(nTriples.join('\n'));
   }
-
-  // extract from the `mu-session-id` the bestuurseenheid the user is asking for (i.e. security measure)
-  const sessionUri = req.headers['mu-session-id'];
-
-  if (!sessionUri ) {
-    return res.status(400).json({
-      error: "Missing mu-session-id. This call should go through mu-identifier."
-    });
+  catch (error) {
+    return res.status(500).json({ error: error.message });
   }
-
-  const fromEenheidUri = await bestuurseenheidForSession(sessionUri);
-
-  // Figure out whether Eenheid is related to CKB
-  const ckbUri = await getRelatedToCKB( forEenheidUri );
-
-  // Get decision type to request
-  const relatedDecisionType = getRelatedDecisionType( decisionType, ckbUri );
-
-  const query = prepareQuery(fromEenheidUri, forEenheidUri, ckbUri, relatedDecisionType );
-
-  // execute query
-  // TODO: Here we could add a hook to connect to vendor-API if we want.
-  const triples = (await querySudo(query))?.results?.bindings || [];
-  const nTriples = triples.map(t => serializeTriple(t)) || [];
-
-  res.set('Content-Type', 'text/turtle');
-  return res.send(nTriples.join('\n'));
 });
 
 
