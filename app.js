d3f6a63dc741293805f750743482bf41831b1074
felix <ruiz.felixruiz@gmail.com>
2024-05-23 11:57:24 +0200

    Update flow forDecision

--- app.js
+++ app.js
@@ -11,19 +11,18 @@ app.get('/hello', function( req, res ) {
 app.get('/related-document-information', async function( req, res ) {
   try {
     // Get the related decisions for specific type & bestuurseenheid provided in the query parameters `?forDecisionType=..&?forEenheid=...
-    const decisionType = req.query.forDecisionType;
-    const forEenheidUri = req.query.forEenheid;
+    const forDecisionType = req.query.forDecisionType;
+    const forEenheid = req.query.forEenheid;
+    const forDecision = req.query.forDecision;
 
-    //TODO add ?forDecision
-
-    // Check if both required parameters are provided
-    if (!decisionType || !forEenheidUri) {
+    // If forDecision is not provided, forDecisionType and forEenheid become mandatory.
+    if (!forDecision && (!forEenheid || !forEenheid)) {
       return res.status(400).json({
         error: "Missing required query parameters. Please provide 'forDecisionType' and 'forEenheid'."
       });
     }
 
-    // extract from the `mu-session-id` the bestuurseenheid the user is asking for (i.e. security measure)
+    // Extract from the `mu-session-id` the bestuurseenheid the user is asking for (i.e. security measure)
     const sessionUri = req.headers['mu-session-id'];
 
     if (!sessionUri ) {
@@ -32,39 +31,50 @@ app.get('/related-document-information', async function( req, res ) {
       });
     }
 
-    const fromEenheidUri = await bestuurseenheidForSession(sessionUri);
-    if(!fromEenheidUri) {
-      return res.status(400).json({
-        error: "No eenheid found for mu-session-id. Aborting"
-      });
+    // If no decision has been provided,
+    //  we need to calculate extra parameters for the query, so we can provide a list of options.
+    let query = '';
+    
+    if(!forDecision) {
+
+      const fromEenheid = await bestuurseenheidForSession(sessionUri);
+      if(!fromEenheid) {
+        return res.status(400).json({
+          error: "No eenheid found for mu-session-id. Aborting"
+        });
+      }
+
+      // Figure out whether Eenheid is related to CKB
+      let ckbUri = await getRelatedToCKB( forEenheid );
+
+      // ------ Start temporary implementation
+      // We currently don't support documents provided by CKB's (mainly the data is broken)
+      // But we still, in development mode, want to make it easier to return some data
+      if(ckbUri && process.env.NODE_ENV != 'development') {
+        console.warn(`CKB ${ckbUri} found for ${forEenheid}, currently case is not supported`);
+        res.set('Content-Type', 'text/turtle');
+        return res.send('');
+      }
+      else if(process.env.NODE_ENV == 'development') {
+        console.warn(`CKB ${ckbUri} found for ${forEenheid}, currently case is not supported, but in development mode, we just ignore this`);
+        ckbUri = null;
+      }
+      // ------ End temporary implementation
+
+      // Get decision type to request
+      const decisionType = getRelatedDecisionType( forDecisionType, ckbUri );
+      if(!decisionType) {
+        return res.status(400).json({
+          error: `No related document/decisionType found ${forDecisionType}. Aborting`
+        });
+      }
+
+      query = prepareQuery({ fromEenheid, forEenheid, ckbUri, decisionType });
     }
 
-    // Figure out whether Eenheid is related to CKB
-    let ckbUri = await getRelatedToCKB( forEenheidUri );
-
-    // ------ Start temporary implmentation
-    // We currently don't support documents provided by CKB's (mainly the data is broken)
-    // But we still, in development mode, want to make it easier to return some data
-    if(ckbUri && process.env.NODE_ENV != 'development') {
-      console.warn(`CKB ${ckbUri} found for ${forEenheidUri}, currently case is not supported`);
-      res.set('Content-Type', 'text/turtle');
-      return res.send('');
+    else {
+      query = prepareQuery({ forDecision } );
     }
-    else if(process.env.NODE_ENV != 'development') {
-      console.warn(`CKB ${ckbUri} found for ${forEenheidUri}, currently case is not supported, but in development mode, we just ignore this`);
-      ckbUri = null;
-    }
-    // ------ End temporary implmentation
-
-    // Get decision type to request
-    const relatedDecisionType = getRelatedDecisionType( decisionType, ckbUri );
-    if(!relatedDecisionType) {
-      return res.status(400).json({
-        error: `No related document/decisionType found ${decisionType}. Aborting`
-      });
-    }
-
-    const query = prepareQuery(fromEenheidUri, forEenheidUri, ckbUri, relatedDecisionType );
 
     // execute query
     // TODO: Here we could add a hook to connect to vendor-API if we need to.
